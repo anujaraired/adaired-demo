b339b526c559794df13f3ccea63a582cf2a2f0a1
anujaraired <anuj@adaired.org>
2025-12-23 11:53:29 +0530

    fic

--- src/app/(website)/(mainWebsite)/services/[slug]/page.tsx
+++ src/app/(website)/(mainWebsite)/services/[slug]/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
-  return (
-    <div>
-      fd
-    </div>
-  )
-}
+  return <div>fd</div>;
+};
 
-export default page
+export default page;
